94f3ba27ff9a904b1dfa88a5a31c3e1be436db64
akinari <akinarix@gmail.com>
2016-12-31 22:24:48 +0800

    add python3 requirements

--- flask-example.py
+++ flask-example.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>gupt-flask-example {Demo}.</h1>"
+    return "<h1>gupt-flask-example Demo.</h1>"
 
 if __name__ == "__main__":
     app.run()
